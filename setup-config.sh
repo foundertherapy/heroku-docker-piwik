048e0be6a3dc85cbcaef8d76e00d8451592c4019
nasief <nasief304@gmail.com>
2016-09-07 17:44:24 +0300

    activate custom dims

--- setup-config.sh
+++ setup-config.sh
@@ -20,5 +20,5 @@ sed -i s/#DB_PREFIX/$DB_PREFIX/ vendor/piwik/piwik/config/config.ini.php
 sed -i s/#SECRET_TOKEN/$SECRET_TOKEN/ vendor/piwik/piwik/config/config.ini.php
 sed -i s/#DOMAIN/$DOMAIN/ vendor/piwik/piwik/config/config.ini.php
 
-composer require symfony/finder
+composer require symfony/console
 ./console plugin:activate CustomDimensions
