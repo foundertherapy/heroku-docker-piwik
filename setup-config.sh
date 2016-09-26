66964c1d180d6ad584e0c37f40a0aa264610244f
nasief <nasief304@gmail.com>
2016-09-26 14:10:37 +0300

    add space

--- setup-config.sh
+++ setup-config.sh
@@ -13,6 +13,7 @@ PIWIK_PATH=./vendor/piwik/piwik/
 CONFIG_PATH="$PIWIK_PATH"config/config.ini.php
 PLUGINS_PATH="$PIWIK_PATH"/plugins/
 
+
 cp config.ini.php "$CONFIG_PATH"
 
 sed -i s/#DB_HOST/$DB_HOST/ $CONFIG_PATH
